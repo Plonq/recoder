81b1ee9bf15f0b94470a11b8490456026c849b5e
Huon Imberger <huon@plonq.org>
2022-06-18 11:37:26 +1000

    site description

--- src/components/header.rs
+++ src/components/header.rs
@@ -4,8 +4,13 @@ use yew::prelude::*;
 #[function_component(Header)]
 pub fn header() -> Html {
     html! {
-        <header>
-            <h1 class="title">{ "ReCoder" }</h1>
-        </header>
+        <>
+            <header>
+                <h1 class="title">{ "ReCoder" }</h1>
+            </header>
+            <p class="site-description">
+                { "A simple tool for text encoding, decoding, and hashing. 100% client-side." }
+            </p>
+        </>
     }
 }
